baa86aed1d7a590aa4bb6b7d756364f6ab5e6626
Kirill Zaidulin <zaidulinkirill@gmail.com>
2021-11-14 19:51:26 +0300

    test

--- src/routes/get.js
+++ src/routes/get.js
@@ -1,40 +1,43 @@
-import sendFile from "koa-send";
-import path from "path";
+import sendFile from 'koa-send';
 
 export default ({
-    model: Model,
-    provider,
-    uploadsFolder,
-    notFoundMiddleware = (ctx, error) => {
-      ctx.body = error;
-      ctx.status = 404;
-    },
-  }) =>
-  async (ctx) => {
-    const { transformString = "", id, format } = ctx.params;
+  model: Model,
+  provider,
+  uploadsFolder,
+  notFoundMiddleware = (ctx, error) => {
+    ctx.body = error;
+    ctx.status = 404;
+  },
+}) => async (ctx) => {
+  console.log('here');
+  const { id } = ctx.params;
 
-    const sendFileConfig = {
-      immutable: true,
-      root: uploadsFolder,
-      ...(ctx.query.range && {
-        setHeaders: (res) => {
-          res.setHeader("Accept-Ranges", "bytes");
-        },
-      }),
-    };
+  console.log(id);
 
-    const file = Model ? await Model.findById(id) : await provider.get(id);
-
-    if (!file) {
-      notFoundMiddleware(ctx);
-      return;
-    }
-
-    try {
-      await sendFile(ctx, file.path, sendFileConfig);
-    } catch (err) {
-      if (ctx.status === 404) {
-        notFoundMiddleware(ctx, err.message);
-      }
-    }
+  const sendFileConfig = {
+    immutable: true,
+    root: uploadsFolder,
+    ...(ctx.query.range && {
+      setHeaders: (res) => {
+        res.setHeader('Accept-Ranges', 'bytes');
+      },
+    }),
   };
+
+  console.log(sendFileConfig);
+
+  const file = Model ? await Model.findById(id) : await provider.get(id);
+
+  if (!file) {
+    notFoundMiddleware(ctx);
+    return;
+  }
+
+  try {
+    await sendFile(ctx, file.path, sendFileConfig);
+  } catch (err) {
+    if (ctx.status === 404) {
+      notFoundMiddleware(ctx, err.message);
+    }
+  }
+};
